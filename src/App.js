e4c3978c20559185eddd119787b7297ecd92de5a
keithchasen <keithchasen89@gmail.com>
2021-06-09 15:49:56 +0300

    apollo provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Container } from "react-bootstrap";
+import { ApolloProvider } from "@apollo/client";
+
+import client from "./ApolloClient";
 import './App.scss';
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <Container className='pt-5'>
-      <Register/>
-    </Container>
+    <ApolloProvider client={client}>
+      <Container className='pt-5'>
+        <Register/>
+      </Container>
+    </ApolloProvider>
   );
 }
 
