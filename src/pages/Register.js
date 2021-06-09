a167d8a126fb64a5cdacff599634d8f2a98b07c7
keithchasen <keithchasen89@gmail.com>
2021-06-09 16:54:08 +0300

    register page uses mutation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,17 +1,38 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from "react-bootstrap";
+import { gql, useMutation } from "@apollo/client";
+
+const REGISTER = gql`
+  mutation register($email: String!, $password: String!, $confirmPassword: String!) {
+      register(email: $email, password: $password, confirmPassword: $confirmPassword) {
+          email
+      }
+  }
+`;
 
 function Register() {
+  const [register, { loading }] = useMutation(REGISTER, {
+    update(_, res) {
+      console.log(res)
+    },
+    onError(err) {
+      console.log(err.graphQLErrors[0].extensions.errors);
+      setErrors(err.graphQLErrors[0].extensions.errors);
+    }
+  });
+
   const [variables, setVariables] = useState({
     email: '',
     password: '',
     confirmPassword: ''
   });
 
+  const [errors, setErrors] = useState({});
+
   const submitRegisterForm = e => {
     e.preventDefault();
 
-    console.log(variables)
+    register({ variables });
   };
 
   return (
@@ -20,30 +41,41 @@ function Register() {
           <h1 className='text-center'>Register</h1>
           <Form onSubmit={submitRegisterForm}>
             <Form.Group>
-              <Form.Label>Email</Form.Label>
+              <Form.Label className={errors.email && 'text-danger'}>
+                {errors.email ?? 'Email'}
+              </Form.Label>
               <Form.Control
                 type='email'
                 value={variables.email}
+                className={errors.email && 'is-invalid'}
                 onChange={e => setVariables({...variables, email: e.target.value})}
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label>Password</Form.Label>
+              <Form.Label className={errors.password && 'text-danger'}>
+                {errors.password ?? 'Password'}
+              </Form.Label>
               <Form.Control
                 type='password'
                 value={variables.password}
+                className={errors.password && 'is-invalid'}
                 onChange={e => setVariables({...variables, password: e.target.value})}
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label>Confirm Password</Form.Label>
+              <Form.Label className={errors.confirmPassword && 'text-danger'}>
+                {errors.confirmPassword ?? 'Confirm Password'}
+              </Form.Label>
               <Form.Control
                 type='password'
                 value={variables.confirmPassword}
+                className={errors.confirmPassword && 'is-invalid'}
                 onChange={e => setVariables({...variables, confirmPassword: e.target.value})}
               />
             </Form.Group>
-            <Button variant="success" type="submit" className='my-2'>Register</Button>
+            <Button variant="success" type="submit" className='my-2' disabled={loading}>
+              {loading ? 'loading...' : 'Register' }
+            </Button>
           </Form>
         </Col>
       </Row>
