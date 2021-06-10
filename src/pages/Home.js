f8e29423fb96a0bd0003bc56a8e879acb887e37f
keithchasen <keithchasen89@gmail.com>
2021-06-10 21:08:51 +0300

    switching between conversations and showing messages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Row, Col, Form, Button, Image } from "react-bootstrap";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
 import avatar from '../media/avatar.png'
 
 const GET_USERS = gql`  
@@ -14,15 +14,26 @@ const GET_USERS = gql`
   }
 `;
 
+const GET_MESSAGES = gql`
+    query getMessages($from: String!) {
+      getMessages(from: $from) {
+          id content from to createdAt
+      }
+}`;
+
 function Home() {
   const { loading, data, error } = useQuery(GET_USERS);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES)
 
-  if (error) {
-    console.log(error)
-  }
+  useEffect(() => {
+    if(selectedUser) {
+      getMessages({ variables: { from: selectedUser }})
+    }
+  }, [selectedUser]);
 
-  if (data) {
-    console.log(data)
+  if (messagesData) {
+    console.log(messagesData, 'mess data')
   }
 
   let usersMarkup = null;
@@ -32,8 +43,13 @@ function Home() {
     usersMarkup = <p>No users</p>
   } else if (data.getUsers.length) {
     usersMarkup = data.getUsers.map(user => (
-      <div className='d-flex p-3' key={user.email}>
-        <Image src={avatar} roundedCircle className="mr-2" style={{ width: 50, height: 50, objectFit: 'cover'}}/>
+      <div className='d-flex p-3' key={user.email} onClick={() => setSelectedUser(user.email)}>
+        <Image
+          src={avatar}
+          roundedCircle
+          className="mr-2"
+          style={{ width: 50, height: 50, objectFit: 'cover'}}
+        />
         <div>
           <p className="text-success">{user.email}</p>
           <p className="font-weight-light">{user.latestMessage ? user.latestMessage.content : ''}</p>
@@ -48,7 +64,10 @@ function Home() {
           {usersMarkup}
         </Col>
         <Col xs={8}>
-          <p>Messages</p>
+          {
+            messagesData && messagesData.getMessages.length ? (
+              messagesData.getMessages.map(message => <p key={message.id}>{message.content}</p>)
+            ) : (<div><p>You're now connected!</p> <p>You can start conversation with {selectedUser}</p></div>)}
         </Col>
       </Row>
   );
