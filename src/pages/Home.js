fc710902274660d983b23440bb2caef10b8e3680
keithchasen <keithchasen89@gmail.com>
2021-06-10 18:16:52 +0300

    users list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,47 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from "react-bootstrap";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_USERS = gql`  
+  query getUsers {
+      getUsers {
+          email
+      }
+  }
+`;
 
 function Home() {
+  const { loading, data, error } = useQuery(GET_USERS);
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (data) {
+    console.log(data)
+  }
+
+  let usersMarkup = null;
+  if (!data || loading) {
+    usersMarkup = <p>Loading...</p>
+  } else if (!data.getUsers.length) {
+    usersMarkup = <p>No users</p>
+  } else if (data.getUsers.length) {
+    usersMarkup = data.getUsers.map(user => (
+      <div key={user.email}>
+        <p>{user.email}</p>
+      </div>
+    ));
+  }
+
   return (
-      <Row className="bg-white py-5 justify-content-center">
-        <h1>HOME</h1>
+      <Row className="bg-white">
+        <Col xs={4}>
+          {usersMarkup}
+        </Col>
+        <Col xs={8}>
+          <p>Messages</p>
+        </Col>
       </Row>
   );
 }
