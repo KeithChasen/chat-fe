856ed328177c80aa15d990935a7ff172d6db0f8b
keithchasen <keithchasen89@gmail.com>
2021-06-12 21:46:48 +0300

    added tooltip with date to message

--- src/pages/home/Message.js
+++ src/pages/home/Message.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { useAuthState } from "../../context/auth";
 import classNames from "classnames";
+import moment from 'moment';
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function Message({ message }) {
   const { user } = useAuthState();
+
+  console.log(message, 'message')
+
   const sent = message.from === user.email;
   const received = !sent;
-  return (
-    <div className={classNames('d-flex my-3', {
-      'ms-auto': sent,
-      'me-auto': received
-    })}>
-      <div className={classNames('p-2 px-3 rounded-pill', {
-        'bg-primary': sent,
-        'bg-secondary': received
+  return (<OverlayTrigger
+    placement={sent ? 'left' : 'right'}
+    overlay={
+    <Tooltip>
+      {moment(new Date(message.createdAt * 1000).toISOString()).format('MMMM DD, YYYY @ h:mm a')}
+    </Tooltip>}
+    >
+      <div className={classNames('d-flex my-3', {
+        'ms-auto': sent,
+        'me-auto': received
       })}>
-        <p className={classNames({'text-white': sent })} key={message.id}>{message.content}</p>
+        <div className={classNames('p-2 px-3 rounded-pill', {
+          'bg-primary': sent,
+          'bg-secondary': received
+        })}>
+          <p className={classNames({'text-white': sent })} key={message.id}>{message.content}</p>
+        </div>
       </div>
-    </div>
+    </OverlayTrigger>
   )
 }
 
